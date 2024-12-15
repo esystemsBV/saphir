340fe59ffbed6332a8d7ae217d2ddf96726e32a2
eSystems <esystems.ma@gmail.com>
2024-12-15 03:12:38 +0100

    Update sqldb.ts

--- backend/src/config/sqldb.ts
+++ backend/src/config/sqldb.ts
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "auth-db1427.hstgr.io",
+  user: "u365232941_saphirweb",
+  password: "99Redzonek1ng",
+  database: "u365232941_saphir",
   connectionLimit: 10,
   queueLimit: 0,
   connectTimeout: 5000,
