25c726816d7130e7a1a4e6d01e09847ba3657168
eSystems <esystems.ma@gmail.com>
2024-12-18 03:59:45 +0100

    Update tailwind.config.js

--- landing/tailwind.config.js
+++ landing/tailwind.config.js
@@ -1,5 +1,3 @@
-const { nextui } = require("@nextui-org/react");
-
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -18,58 +16,10 @@ export default {
         },
       },
       colors: {
-        primary: {
-          50: "#e6faf5",
-          100: "#ccf5eb",
-          200: "#99ead7",
-          300: "#66e0c3",
-          400: "#33d5af",
-          500: "#01CBA5",
-          600: "#00b594",
-          700: "#008f73",
-          800: "#006a52",
-          900: "#004531",
-        },
         main: "#B12B89",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        chart: {
-          1: "hsl(var(--chart-1))",
-          2: "hsl(var(--chart-2))",
-          3: "hsl(var(--chart-3))",
-          4: "hsl(var(--chart-4))",
-          5: "hsl(var(--chart-5))",
-        },
       },
     },
   },
-  darkMode: "class",
+  darkMode: "",
   plugins: [require("tailwindcss-animate")],
 };
